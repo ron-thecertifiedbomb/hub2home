33c7c75c6598617adcd2d0a4edeaf81b05d2ff79
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2023-03-08 10:34:54 +0800

    added succsessful page and navlinks hover

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,13 +3,13 @@ import React from "react";
 const Hero = ({title}) => {
     
   return (
-    <div className="w-full h-[516px] bg-[#FFF7F3] pt-[56px]">
-      <div className="flex bg-[#FFF7F3] max-w-[1140px] w-full h-[100%] justify-center items-center m-auto">
+    <main className="w-full h-[516px] bg-[#FFF7F3] pt-[56px]">
+      <section className="flex bg-[#FFF7F3] max-w-[1140px] w-full h-[100%] justify-center items-center m-auto">
         <h1 className="font-Roboto text-[#f02f1b] text-[30px] md:text-[60px] font-bold tracking-normal  ">
           {title}
         </h1>
-      </div>
-    </div>
+      </section>
+    </main>
   );
 };
 
---
+++ src/components/SuccessfulPageHero/SuccessfulPageHero.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Logo from "../../assets/h2h-logo.svg";
+import Title from "../../assets/h2h-lm (1).svg";
+
+const SuccessfulPageHero = () => {
+
+  return (
+    <section className="  flex flex-col gap-4 justify-center items-center">
+      <img className=" w-[40px] md:w-[60px]" src={Logo} alt="logo"></img>
+      <img className=" w-[180px] md:w-[230px]" src={Title} alt="logo"></img>
+
+      <div className="flex flex-col items-center mt-[84px]">
+        <h1 className=" font-Roboto text-[25px] md:text-[31px] font-bold text-[#fa7419] tracking-tight">
+          Thank you for reaching out!
+        </h1>
+        <h1 className=" mt-[24px] font-Roboto text-[11px] md:text-[18px] font-normal  text-[#ffffff tracking-tight">
+          We received your message and we'll get back to you as soon as we can!
+        </h1>
+        <button className=" flex w-full md:w-[240px] h-[52px] justify-center items-center bg-[#f02f1b] mt-[46px] rounded "><h1 className=" font-Roboto text-[16px] tracking-normal text-white">Go back to home page</h1></button>
+      </div>
+    </section>
+  );
+};
+
+export default SuccessfulPageHero;
