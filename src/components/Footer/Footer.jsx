c160ecb52e4359ba9a26e25b66e0c500e5596de0
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2023-03-09 16:53:47 +0800

    sdasdsa

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <main className=' flex h-[381px] w-full bg-[#f02f1b] justify-center items-center'>
+       <section className='max-w-[1140px] w-full'>
+        </section> 
+        </main>
+  )
+}
+
+export default Footer
