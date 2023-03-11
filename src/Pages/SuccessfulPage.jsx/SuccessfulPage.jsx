2d584cf3b82d64a5cd89cf9500431ebea64353d2
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2023-03-11 11:57:41 +0800

    fixed

--- src/Pages/SuccessfulPage.jsx/SuccessfulPage.jsx
+++ src/Pages/SuccessfulPage.jsx/SuccessfulPage.jsx
@@ -3,8 +3,8 @@ import SuccessfulPageHero from "../../components/SuccessfulPageHero/SuccessfulPa
 import GoToTop from "../../components/GoToTop/GoToTop";
 const SuccessfulPage = () => {
   return (
-    <main className="w-full h-screen bg-[#FFF7F3]">
-      <section className="flex justify-center items-center max-w-[1140px] w-full h-screen m-auto bg-[#FFF7F3]">
+    <main className="w-full h-screen bg-[#ffffff]">
+      <section className="flex justify-center items-center max-w-[1140px] w-full h-screen m-auto bg-[#ffffff]">
         <SuccessfulPageHero />
         <GoToTop />
       </section>
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Logo from "../../assets/logo.svg";
 import styles from "../../constants/style";
 import { navLinks } from "../../constants/navlink";
@@ -7,41 +7,58 @@ import { RxHamburgerMenu } from "react-icons/rx";
 import { AiOutlineClose } from "react-icons/ai";
 
 const NavBar = () => {
-
-const [showMenu, setShowMenu] = useState(false)
+  const [showMenu, setShowMenu] = useState(false);
 
   return (
     <section className="w-full bg-[#FFF7F3] h-[60px] fixed z-10 top-0">
       <nav className=" flex max-w-[1140px] w-full h-[60px] justify-between items-center m-auto p-3 bg-[#FFF7F3] fixed top-0 left-0 right-0">
         <img src={Logo} alt="logo" className="w-[150px] md:w-[204px]"></img>
         <section className="flex justify-center items-center gap-1 relative">
-          <ul className="hidden md:flex gap-[22px] ">
+         
+          <section className="hidden md:flex gap-[22px]">
             {navLinks.map((item, index) => (
-              <NavLink to={item.to}>
-                <li className={styles.link} key={index}>
-                  {item.link}
-                </li>
-              </NavLink>
+              <ul className="hidden md:flex gap-[22px]" key={index}>
+                <NavLink to={item.to}>
+                  <li className={styles.link}>{item.link}</li>
+                </NavLink>
+              </ul>
             ))}
-          </ul>
-          <div className="flex justify-center items-center pb-1 md:hidden " onClick={() => {setShowMenu(!showMenu)}}>
-          {showMenu ? <AiOutlineClose style={{ fontSize: "25", color: "black" }} />: <RxHamburgerMenu  style={{ fontSize: "25", color: "black" }} />}
+          </section>
+
+          <div
+            className="flex justify-center items-center pb-1 md:hidden "
+            onClick={() => {
+              setShowMenu(!showMenu);
+            }}
+          >
+            {showMenu ? (
+              <AiOutlineClose style={{ fontSize: "25", color: "black" }} />
+            ) : (
+              <RxHamburgerMenu style={{ fontSize: "25", color: "black" }} />
+            )}
           </div>
         </section>
       </nav>
-          <div className={showMenu ? styles.show : styles.hide}>
-            <section>
-          <ul className="md:flex gap-[22px] ">
+      <div className={showMenu ? styles.show : styles.hide}>
+
+        <section>
             {navLinks.map((item, index) => (
+          <ul className="md:flex gap-[22px] " key={index} >
               <Link to={item.to}>
-                <li className={styles.sublink} key={index} onClick={() => {setShowMenu(false)}}>
+                <li
+                  className={styles.sublink}
+                  
+                  onClick={() => {
+                    setShowMenu(false);
+                  }}
+                >
                   {item.link}
                 </li>
               </Link>
-            ))}
           </ul>
-            </section>
-          </div>
+            ))}
+        </section>
+      </div>
     </section>
   );
 };
