e94f565c97f0980c9432cb8fd4954134de7d0edc
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2023-03-15 18:57:40 +0800

    added

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/hub2home/',
   build: {
     chunkSizeWarningLimit: 1600,
   },
